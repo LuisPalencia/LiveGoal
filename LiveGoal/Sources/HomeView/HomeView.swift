2cea7a4b0de8649c54a1a5c93b4b5752f989ca96
Luis Palencia <luis.palencia525@gmail.com>
2022-04-28 19:01:55 +0200

    [WIP] - Modifies the TabView in the HomeView in order to add the correct icons

--- LiveGoal/Sources/HomeView/HomeView.swift
+++ LiveGoal/Sources/HomeView/HomeView.swift
@@ -9,38 +9,54 @@ import SwiftUI
 
 struct HomeView: View {
     
-    @StateObject private var viewModel = HomeViewModel()
+    @State var selectedTabItem: TabItemViewModel.TabItemType = .standing
     
     var body: some View {
-        TabView(selection: self.$viewModel.selectedTabItem,
+        TabView(selection: self.$selectedTabItem,
                 content:  {
-                    ForEach(self.viewModel.tabItemViewModels, id: \.self){ item in
-                        tabView(tabItemType: item.type)
-                            .tabItem {
-                                Image(systemName: item.imageName)
-                                Text(item.title)
-                            }.tag(item.type)
-                    }
+                    LeagueClassificationCoordinator.navigation()
+                     .tabItem {
+                        Image("standing")
+                            .renderingMode(.template)
+                            .foregroundColor(.green)
+                        Text("Standing")
+                     }
+                        .tag(TabItemViewModel.TabItemType.standing)
+                    
+                    LeagueRoundsCoordinator.navigation()
+                     .tabItem {
+                        Image(systemName: "calendar")
+                        Text("Games")
+                     }
+                        .tag(TabItemViewModel.TabItemType.games)
+                    RankingsCoordinator.navigation()
+                     .tabItem {
+                        Image("ranking")
+                            .renderingMode(.template)
+                        Text("Ranking")
+                     }
+                        .tag(TabItemViewModel.TabItemType.top)
+                    
+                    FavouritesCoordinator.navigation()
+                     .tabItem {
+                        Image(systemName: "heart.fill")
+                        Text("Favourites")
+                     }
+                        .tag(TabItemViewModel.TabItemType.favourites)
+                    
+                    ProfileView()
+                     .tabItem {
+                        Image(systemName: "person.fill")
+                        Text("Profile")
+                     }
+                        .tag(TabItemViewModel.TabItemType.profile)
+                    
                 })
             .accentColor(.red)
             //.environment(\.colorScheme, .dark)
     }
     
-    @ViewBuilder
-    func tabView(tabItemType: TabItemViewModel.TabItemType) -> some View {
-        switch tabItemType {
-        case .standing:
-            LeagueClassificationCoordinator.navigation()
-        case .games:
-            LeagueRoundsCoordinator.navigation()
-        case .top:
-            RankingsCoordinator.navigation()
-        case .favourites:
-            FavouritesCoordinator.navigation()
-        case .profile:
-            ProfileView()
-        }
-    }
+    
 }
 
 struct HomeView_Previews: PreviewProvider {
